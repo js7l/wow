868bc0f71b943fe9f43462e17de626eab51abcfd
Christina Sugiono <christina.sugiono95@gmail.com>
2022-03-07 15:07:41 +0800

    added validations and association

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,7 @@
 class Studio < ApplicationRecord
   belongs_to :user
+  has_many :sessions, dependent: :destroy
+
+  validates :address, :name, :email, :phone_number, :description, presence: true
+  validates :user, uniqueness: true
 end
