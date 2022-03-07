1ff16d3d408fdb932764f3af9e83e997d1916146
Ratna Purwati <ratna.purwati@ymail.com>
2022-03-07 14:51:36 +0800

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, except: [:create] do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:show, :edit, :update] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
+
+  resources :studios, only: [:new, :show, :create, :edit, :update] do
+    resources :sessions, only: [:create]
+  end
+
+  get '/dashboard', to: 'pages#dashboard'
 end
