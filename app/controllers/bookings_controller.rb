d186de69cc6a3bdecc2bd7e7e1203d5a1b520e3a
Ratna Purwati <ratna.purwati@ymail.com>
2022-03-09 17:37:54 +0800

    show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,16 @@
 class BookingsController < ApplicationController
 
-  def show
-    @event = Event.find(params[:event_id])
-    @bookings = Booking.find(params[:id])
-  end
-
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
     @event = Event.find(params[:event_id])
     @booking.user = current_user
-    @booking.total_price = @item.price * (@booking.end_date - @booking.start_date).to_i
-    @booking.status = "Pending"
+    @booking.total_price = @event.price
+    @booking.status = "Confirmed"
     @booking.event = @event
     if @booking.save
       redirect_to dashboard_path
     else
-      render "event/show"
+      render "events/show"
     end
   end
 
@@ -27,9 +22,5 @@ class BookingsController < ApplicationController
 
   end
 
-  private
 
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
-  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :status, :total_price, :total_attendees, presence: true
+  validates :status, :total_price, presence: true
 end
