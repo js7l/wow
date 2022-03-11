02547d4eb9672697bca414660a4432c1317ee47c
jessica wong <jsica7@gmail.com>
2022-03-11 15:22:01 +0800

    studio

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :studios, dependent: :destroy
   has_many :chatrooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
   has_one_attached :avatar
   acts_as_favoritor
 
