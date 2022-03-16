e0c0a5b308bc9488e469cb196cb65d59abc2262c
Christina Sugiono <christina.sugiono95@gmail.com>
2022-03-16 15:17:53 +0800

    add pay now pay later

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,24 +12,31 @@ class BookingsController < ApplicationController
     @booking.amount = @event.price
     @booking.status = "Pending"
     @booking.event = @event
-    if @booking.save
-      # redirect_to dashboard_path
-      session = Stripe::Checkout::Session.create(
-        payment_method_types: ['card'],
-        line_items: [{
-          name: @event.name,
-          images: [@event.photo.service_url],
-          amount: @event.price_cents,
-          currency: 'usd',
-          quantity: 1
-        }],
-        success_url: dashboard_url,
-        cancel_url: dashboard_url
-      )
-      @booking.update(checkout_session_id: session.id)
-      redirect_to new_booking_payment_path(@booking)
+
+    if params[:paynow]
+      if @booking.save
+        session = Stripe::Checkout::Session.create(
+          payment_method_types: ['card'],
+          line_items: [{
+            name: @event.name,
+            images: [@event.photo.service_url],
+            amount: @event.price_cents,
+            currency: 'usd',
+            quantity: 1
+          }],
+          success_url: dashboard_url,
+          cancel_url: dashboard_url
+        )
+        @booking.update(checkout_session_id: session.id)
+        redirect_to new_booking_payment_path(@booking)
+      else
+        render "events/show"
+      end
     else
-      render "events/show"
+      @booking.status = "Cash"
+      if @booking.save
+        redirect_to dashboard_path
+      end
     end
   end
 
