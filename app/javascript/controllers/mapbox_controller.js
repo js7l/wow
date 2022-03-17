c8d9432d2043d558176769ca0cdb82ed2bee6953
Ratna Purwati <ratna.purwati@ymail.com>
2022-03-17 12:57:45 +0800

    styling map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -203,9 +203,9 @@ export default class extends Controller {
     for (const step of steps) {
       tripInstructions += `<li>${step.maneuver.instruction}</li>`;
     }
-    instructions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
+    instructions.innerHTML = `<p><strong>Trip Duration: ${Math.floor(
       data.duration / 60
-    )} min 🚶‍♂️ </strong></p><ol>${tripInstructions}</ol>`;
+    )} mins 🚗 </strong></p><ol>${tripInstructions}</ol>`;
   }
 
   // create a function to make a directions request
@@ -215,7 +215,7 @@ export default class extends Controller {
     // only the end or destination will change
     const start = [115.130468, -8.654085];
     const query = await fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/walking/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
       { method: 'GET' }
     );
     const json = await query.json();
