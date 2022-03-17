bc36fba4b73ef770dd899a35e5d9ae191279754f
jessica wong <jsica7@gmail.com>
2022-03-17 14:49:38 +0800

    solve conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,17 +14,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :first_name, uniqueness: { scope: :last_name }
 
-<<<<<<< HEAD
-  def chatroom
-    @client_chatrooms = current_user.chatrooms # As a client
-    @studio_chatrooms = Chatroom.where(studio: current_user.studios) # As a studio owner
-    @chatrooms = (@client_chatrooms + @studio_chatrooms).uniq
-  end
-
-  def unread_messages
-    Message.joins(:chatroom).where(read: false, chatrooms: { user: self }).or(Message.joins(:chatroom).where(read: false, chatrooms: { user: self })).count
-  end
-=======
   # def chatrooms
   #   @client_chatrooms = current_user.chatrooms # As a client
   #   @studio_chatrooms = Chatroom.where(studio: current_user.studios) # As a studio owner
@@ -34,5 +23,4 @@ class User < ApplicationRecord
   # def unread_messages
   #   Message.joins(:chatroom).where(read: false, chatrooms: { user: @studio }).or(Message.joins(:chatroom).where(read: false, chatrooms: { user: @client })).count
   # end
->>>>>>> ca746f69bd7a1a116da2caa97f7fee14a26624ad
 end
